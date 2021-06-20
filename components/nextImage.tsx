757d43cbf2f61c5504dfd0b53d8a3e734d0f1067
An Xuan Hoang <xuanan2001@gmail.com>
2021-06-20 17:12:29 +0700

    Contemporary solution to the vertical image problem

--- components/nextImage.tsx
+++ components/nextImage.tsx
@@ -6,11 +6,17 @@ type Props = {
 };
 
 const NextImage = ({ node }: Props): JSX.Element => {
-  const imageProps = {
-    ...node.properties, // img tag attribute
-    ...node.imageDimensions, // width and height
-  };
-  return <Image {...imageProps} />;
+  const isVertical = node.imageDimensions.height > node.imageDimensions.width;
+
+  if (isVertical) {
+    return (
+      <div className={"vertical-image-div md:aspect-w-3 md:aspect-h-2"}>
+        <Image className="vertical-image" layout="fill" {...node.properties} />
+      </div>
+    );
+  }
+
+  return <Image {...node.properties} {...node.imageDimensions} />;
 };
 
 export default NextImage;
