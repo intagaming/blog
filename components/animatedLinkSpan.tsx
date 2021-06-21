bc42db9c345db62afd2195166a081380dea78324
An Xuan Hoang <xuanan2001@gmail.com>
2021-06-21 18:29:06 +0700

    Switch Next.js link to <Link> instead of <a>

---
+++ components/animatedLinkSpan.tsx
@@ -0,0 +1,34 @@
+import { animated, useSpring, config } from "react-spring";
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+const AnimatedLinkSpan = ({ children }: Props): JSX.Element => {
+  const [{ opacity }, api] = useSpring(() => ({
+    opacity: 0,
+    config: config.gentle,
+  }));
+
+  return (
+    <animated.span
+      onMouseOver={() => {
+        api.start({ opacity: 0.4 });
+      }}
+      onMouseLeave={() => {
+        api.start({ opacity: 0 });
+      }}
+      style={{
+        cursor: "pointer",
+        backgroundColor: opacity.to(
+          (opacity) => `hsl(21, 27%, 45%, ${opacity})`
+        ),
+      }}
+    >
+      <span className="text-indigo-600 underline">{children}</span>
+    </animated.span>
+  );
+};
+
+export default AnimatedLinkSpan;
--- components/mylink.tsx
+++ components/mylink.tsx
@@ -1,7 +1,7 @@
 import { Element } from "hast";
 import Link, { LinkProps } from "next/link";
 import { ReactNode } from "react";
-import { animated, useSpring, config } from "react-spring";
+import AnimatedLinkSpan from "./animatedLinkSpan";
 
 type Props = {
   href: LinkProps["href"];
@@ -10,29 +10,22 @@ type Props = {
 };
 
 const MyLink = ({ href, children }: Props): JSX.Element => {
-  const [{ opacity }, api] = useSpring(() => ({
-    opacity: 0,
-    config: config.gentle,
-  }));
+  const isNextLink = href.toString().startsWith("/");
+
+  if (isNextLink) {
+    return (
+      <Link href={href}>
+        <a>
+          <AnimatedLinkSpan>{children}</AnimatedLinkSpan>
+        </a>
+      </Link>
+    );
+  }
+
   return (
-    <Link href={href}>
-      <animated.a
-        onMouseOver={() => {
-          api.start({ opacity: 0.4 });
-        }}
-        onMouseLeave={() => {
-          api.start({ opacity: 0 });
-        }}
-        style={{
-          cursor: "pointer",
-          backgroundColor: opacity.to(
-            (opacity) => `hsl(21, 27%, 45%, ${opacity})`
-          ),
-        }}
-      >
-        <span className="text-indigo-600 underline">{children}</span>
-      </animated.a>
-    </Link>
+    <a href={href.toString()}>
+      <AnimatedLinkSpan>{children}</AnimatedLinkSpan>
+    </a>
   );
 };
 
