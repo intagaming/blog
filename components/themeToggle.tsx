5dde318fceb97ceae5d30f4a4ecb63f39073485e
An Xuan Hoang <xuanan2001@gmail.com>
2021-07-07 01:48:31 +0700

    Fix dark theme icon inverse display

--- components/themeToggle.tsx
+++ components/themeToggle.tsx
@@ -22,29 +22,14 @@ export default function ThemeToggle(): JSX.Element {
     <Switch
       checked={enabled}
       onChange={toggle}
-      className="focus:outline-none text-white flex items-center"
+      className="flex items-center text-white focus:outline-none"
     >
       <span className="sr-only">Enable Dark mode</span>
       <span>
         {enabled ? (
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-            />
-          </svg>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
+            className="w-6 h-6"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -54,6 +39,21 @@ export default function ThemeToggle(): JSX.Element {
               strokeLinejoin="round"
               strokeWidth={2}
               d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
             />
           </svg>
         )}
