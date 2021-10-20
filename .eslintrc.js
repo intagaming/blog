cf7d27baabeb08b1df9fe7a181f184504677dad5
An Xuan Hoang <xuanan2001@gmail.com>
2021-10-20 23:34:14 +0700

    fix: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   extends: ["airbnb-typescript", "next", "prettier"],
   rules: {
     "no-param-reassign": ["error", { props: false }],
-    "import/no-extraneous-dependencies": ["error", { "devDependencies": true }]
+    "import/no-extraneous-dependencies": ["error", { "devDependencies": true }],
+    "@next/next/no-document-import-in-page": "off"
   },
   parserOptions: {
     project: ["./tsconfig.json"]
