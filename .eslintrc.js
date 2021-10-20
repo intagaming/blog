73b63b6fb1756fad450b3dcb3d38fde56e3f6b80
An Xuan Hoang <xuanan2001@gmail.com>
2021-10-20 23:16:22 +0700

    refactor: switch to old school yarn

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,9 @@ module.exports = {
   extends: ["airbnb-typescript", "next", "prettier"],
   rules: {
     "no-param-reassign": ["error", { props: false }],
+    "import/no-extraneous-dependencies": ["error", { "devDependencies": true }]
   },
   parserOptions: {
-    project: "./tsconfig.json",
-  },
+    project: ["./tsconfig.json"]
+  }
 };
